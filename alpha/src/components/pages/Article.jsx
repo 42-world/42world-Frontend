b69aa1ba6d7b00732a1404da45c8020fae55ea05
PIut0 <slimdoks@gmail.com>
2022-03-23 22:22:13 +0900

    Feat: 게시글 페이지 생성 및 라우터에 추가

---
+++ alpha/src/components/pages/Article.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styled from "styled-components";
+import { Container } from "../atoms/global";
+
+const Article = () => {
+  return <ArticleBlock>AuthForm</ArticleBlock>;
+};
+
+const ArticleBlock = styled(Container)``;
+
+export default Article;
