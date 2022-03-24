9be059884d90edc4c7090bab86a828b18e82a3dd
PIut0 <slimdoks@gmail.com>
2022-03-24 22:09:03 +0900

    Feat: 작성 페이지 추가 및 컴포넌트 연결

---
+++ alpha/src/components/organisms/writing/WritingContent.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+const WritingContent = () => {
+  return <WritingContentBlock>AuthForm</WritingContentBlock>;
+};
+
+const WritingContentBlock = styled.div``;
+
+export default WritingContent;
