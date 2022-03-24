6a57263f4b81b1373247aa55b835116e3d13134a
GulSam00 <gulsamcono@gmail.com>
2022-03-24 12:46:12 +0900

    Feat : 목업 데이터 추가.

--- alpha/src/components/organisms/community/Board.jsx
+++ alpha/src/components/organisms/community/Board.jsx
@@ -1,10 +1,77 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import {
+  // ArticleList,
+  // ArticleTitle,
+  // Body,
+  // CountDisplay,
+  // CountItem,
+  // Head,
+  // TitleSide,
+  // Wrapper,
+  BoardContainer
+} from "../../atoms/Board";
 import styled from "styled-components";
 
-const Board = () => {
-  return <CommunityBlock>광고</CommunityBlock>;
+const getData = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(
+      () =>
+        resolve([
+          {
+            title: "success",
+            content: "샘플 데이터",
+            writer: "afs",
+            createTime: "2022-03-24",
+            viewCount: 124,
+            likeCount: 16,
+            commentCount: 4
+          },
+          {
+            title: "success",
+            content: "샘플 데이터",
+            writer: "afs",
+            createTime: "2022-03-24",
+            viewCount: 124,
+            likeCount: 16,
+            commentCount: 4
+          }
+        ]),
+      1000
+    );
+  });
 };
 
-const CommunityBlock = styled.div``;
+const Board = () => {
+  const [Articles, setArticles] = useState(null);
+
+  const getArticles = async () => {
+    const result = await getData();
+    setArticles(result);
+  };
+  useEffect(() => {
+    getArticles();
+  }, []);
+  return (
+    <CommunityBlock>
+      <div className="header">
+        <h1>자유게시판</h1> <h2>검색</h2>
+        <button>글 쓰기</button>
+      </div>
+      <BoardContainer>
+        {Articles &&
+          Articles.map(article => {
+            return <div>{article.content}</div>;
+          })}
+      </BoardContainer>
+    </CommunityBlock>
+  );
+};
+
+const CommunityBlock = styled.div`
+  .header {
+    display: flex;
+    flex-direction: row;
+  }
+`;
 
 export default Board;
