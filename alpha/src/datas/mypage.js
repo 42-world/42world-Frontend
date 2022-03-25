30bd03841000926fe0b0986442c4148f06a65a5d
chichoon <chichoon.choi@gmail.com>
2022-03-25 12:19:22 +0900

    Feat: Mypage rawdata 세팅
    - 필요해 보이는 것만 일단 추가

---
+++ alpha/src/datas/mypage.js
@@ -0,0 +1,10 @@
+const MypageData = {
+  userName: "echung",
+  profilePhoto: "https://bit.ly/3D49HWk",
+  links: {
+    github: "https://github.com",
+    intra42: "https://profile.intra.42.fr",
+  },
+};
+
+export default MypageData;
