ebe07b7c299335462a5305e8c2321ebb21f78810
PIut0 <slimdoks@gmail.com>
2022-04-06 00:38:45 +0900

    Refactor: box-shadow 속성을 theme 으로 이전 및 전역 스타일 적용

--- alpha/src/components/organisms/article/Comment.jsx
+++ alpha/src/components/organisms/article/Comment.jsx
@@ -93,7 +93,7 @@ const CommentBlock = styled.div`
   width: 100%;
   padding: 1rem;
   background-color: #fff;
-  box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+  box-shadow: ${(props) => props.theme.boxShadow};
   border-radius: 0.3rem;
   .comment_info {
     display: flex;
--- alpha/src/components/organisms/community/CommunityList.jsx
+++ alpha/src/components/organisms/community/CommunityList.jsx
@@ -34,7 +34,7 @@ const CommunityListBlock = styled.div`
   width: 100%;
   background-color: #fff;
   padding: 1rem;
-  box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+  box-shadow: ${(props) => props.theme.boxShadow};
   border-radius: 0.4rem;
 
   h2 {
