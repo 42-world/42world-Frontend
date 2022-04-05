f7ddfaa1cf0a1f0e459e49a5ff61568565b2bcce
PIut0 <slimdoks@gmail.com>
2022-04-05 22:56:06 +0900

    Refactor: 메인페이지 클러스터 정보 컴포넌트 변경

--- alpha/src/components/organisms/main/ClusterStatus.jsx
+++ alpha/src/components/organisms/main/ClusterStatus.jsx
@@ -3,38 +3,72 @@ import styled from "styled-components";
 import { CheckInService } from "../../../network";
 import { rem } from "../../../styles/rem";
 
+import Button from "@mui/material/Button";
+import { CircularProgressbar } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
+
 function ClusterStatus() {
-  const [status, setStatus] = useState();
+  const [checkInStatus, setCheckInStatus] = useState({
+    max: {
+      gaepo: 1,
+      seocho: 1,
+    },
+    now: {
+      gaepo: 0,
+      seocho: 0,
+    },
+  });
+
+  const getCheckInText = (now, max) => (now ? now + "/" + max : "No Data");
 
   const fetch = async () => {
     const response = await CheckInService.getClusterStatus();
-    setStatus(response.data);
+    setCheckInStatus(response.data);
   };
 
   useEffect(() => {
     fetch();
   }, []);
 
-  const handleClickClusterStatus = () => {
-    window.open("https://cluster.42seoul.io/");
-  };
-
-  if (!status) return <></>;
+  if (!checkInStatus) return <></>;
   return (
-    <ClusterStatusWrapper onClick={handleClickClusterStatus}>
-      <div className="title">클러스터 현황</div>
-      <div className="progress-container">
-        <ProgressBar
-          cluster="개포"
-          total={status.max.gaepo}
-          current={status.now.gaepo}
-        />
-        <ProgressBar
-          cluster="서초"
-          total={status.max.seocho}
-          current={status.now.seocho}
-        />
-      </div>
+    <ClusterStatusWrapper>
+      <CheckInHeader>
+        <p>클러스터 현황</p>
+        <a href="https://cluster.42seoul.io/submit">
+          <Button variant="contained">체크인</Button>
+        </a>
+      </CheckInHeader>
+      <CheckInBody>
+        <div className="circular_progress_bar_div gaepo">
+          <h2>개포</h2>
+          <div className="circular_progress_bar">
+            <CircularProgressbar
+              maxValue={checkInStatus.max.gaepo ? checkInStatus.max.gaepo : 1}
+              text={getCheckInText(
+                checkInStatus.now.gaepo,
+                checkInStatus.max.gaepo
+              )}
+              value={checkInStatus.now.gaepo}
+              strokeWidth={12}
+            />
+          </div>
+        </div>
+        <div className="circular_progress_bar_div seocho">
+          <h2>서초</h2>
+          <div className="circular_progress_bar">
+            <CircularProgressbar
+              maxValue={checkInStatus.max.seocho ? checkInStatus.max.seocho : 1}
+              text={getCheckInText(
+                checkInStatus.now.seocho,
+                checkInStatus.max.seocho
+              )}
+              value={checkInStatus.now.seocho}
+              strokeWidth={12}
+            />
+          </div>
+        </div>
+      </CheckInBody>
     </ClusterStatusWrapper>
   );
 }
@@ -149,6 +183,88 @@ const ProgressBarWrapper = styled.div`
     align-items: center;
     font-size: ${rem(12)};
     font-weight: bold;
+  }
+`;
+
+const CheckInHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  background-color: ${(props) => props.theme.backgroundBlue1};
+  flex-direction: row;
+  align-items: center;
+  padding: 0.5rem 1.2rem;
+  p {
+    font-size: 1rem;
+    font-weight: 600;
+  }
+  a {
+    text-decoration: none;
+    outline: none;
+    border: none;
+  }
+  button {
+    padding: 0.2rem 0.5rem;
+    background-color: ${(props) => props.theme.buttonBlue2};
+  }
+`;
+
+const CheckInBody = styled.div`
+  display: flex;
+  justify-content: space-around;
+  padding: 0.5rem 1.2rem 1.8rem;
+  background-color: ${(props) => props.theme.backgroundBlue1};
+
+  .circular_progress_bar_div {
+    width: 7.3rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+
+    h2 {
+      font-size: 1.1rem;
+      font-weight: 700;
+      margin-bottom: 0.3rem;
+      color: ${(props) => props.theme.textBlack};
+    }
+
+    .circular_progress_bar {
+      svg {
+        path {
+          stroke-linecap: butt;
+        }
+        text {
+          font-size: 0.9rem;
+          font-weight: 600;
+        }
+      }
+    }
+  }
+
+  .gaepo {
+    .CircularProgressbar-trail {
+      stroke: ${(props) => props.theme.backgroundGray3}80;
+    }
+    .CircularProgressbar-path {
+      stroke: ${(props) => props.theme.primary};
+    }
+    .CircularProgressbar-text {
+      fill: ${(props) => props.theme.textBlack};
+      //fill: ${(props) => props.theme.primary};
+    }
+  }
+
+  .seocho {
+    .CircularProgressbar-trail {
+      stroke: ${(props) => props.theme.backgroundGray3}80;
+    }
+    .CircularProgressbar-path {
+      stroke: ${(props) => props.theme.primaryEcole};
+    }
+    .CircularProgressbar-text {
+      fill: ${(props) => props.theme.textBlack};
+      //fill: ${(props) => props.theme.primaryEcole};
+    }
   }
 `;
 
