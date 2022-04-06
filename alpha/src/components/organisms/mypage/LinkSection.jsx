caf2711ae0538fba4958f45a461f441683638f60
chichoon <chichoon.choi@gmail.com>
2022-04-06 17:03:56 +0900

    Feat: Link Section 추가

---
+++ alpha/src/components/organisms/mypage/LinkSection.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styled from "styled-components";
+
+import IconSet from "../../atoms/Mypage";
+import MypageData from "../../../datas/mypage";
+
+const LinkSection = () => {
+  return <div>hello</div>;
+};
+
+export default LinkSection;
