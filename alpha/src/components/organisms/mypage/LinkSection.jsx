d97415fe361e875bfb01ac87f2d843aaedea58fb
chichoon <chichoon.choi@gmail.com>
2022-04-08 11:21:19 +0900

    Delete: 안 쓰는 임포트 헤더 삭제

--- alpha/src/components/organisms/mypage/LinkSection.jsx
+++ alpha/src/components/organisms/mypage/LinkSection.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import styled from "styled-components";
 
 import IconSet from "../../atoms/Mypage";
-import MypageData from "../../../datas/mypage";
 
 const LinkSection = ({ links }) => {
   const GetIcon = (linkType) => {
