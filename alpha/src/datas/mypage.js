074f914e4f144a613ce820e94f8086c12305f9e1
chichoon <chichoon.choi@gmail.com>
2022-04-08 11:00:54 +0900

    Feat: Solved.ac 아이콘 및 링크 추가

--- alpha/src/datas/mypage.js
+++ alpha/src/datas/mypage.js
@@ -4,6 +4,10 @@ const MypageData = {
   links: {
     github: ["https://github.com", "euiminnn"],
     intra42: ["https://profile.intra.42.fr", "echung"],
+    linkedin: ["https://kr.linkedin.com/", "asd"],
+    facebook: ["https://facebook.com", "123"],
+    twitter: ["https://twitter.com", "asdasd"],
+    solvedac: ["https://solved.ac", "baekjoon"],
   },
 };
 
