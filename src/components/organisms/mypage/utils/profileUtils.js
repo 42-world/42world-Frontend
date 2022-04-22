8aefd68ac6c2ede7e3795e3248ddf0964331926b
chichoon <chichoon.choi@gmail.com>
2022-04-22 18:57:16 +0900

    Feat: 보라돌이뚜비나나뽀 순서변경

--- src/components/organisms/mypage/utils/profileUtils.js
+++ src/components/organisms/mypage/utils/profileUtils.js
@@ -1,14 +1,14 @@
 const PROFILE_LIST = [
-  { id: 0, image: "bbo.png" },
   { id: 1, image: "bora.png" },
+  { id: 8, image: "babybora.png" },
   { id: 2, image: "ddub.png" },
+  { id: 9, image: "babyddub.png" },
   { id: 3, image: "nana.png" },
+  { id: 7, image: "babynana.png" },
+  { id: 0, image: "bbo.png" },
+  { id: 6, image: "babybbo.png" },
   { id: 4, image: "bongsuni.png" },
   { id: 5, image: "hyeonkim.png" },
-  { id: 6, image: "babybbo.png" },
-  { id: 7, image: "babynana.png" },
-  { id: 8, image: "babybora.png" },
-  { id: 9, image: "babyddub.png" },
   { id: 10, image: "babyhyeonkim.png" },
 ];
 
