aeb06368abc975a1ea202029285c57ac5942e47b
chichoon <chichoon.choi@gmail.com>
2022-04-28 12:54:27 +0900

    Feat: Auth Button 대략적인 컴포넌트 갖고옴 (from 42world 기존)

--- src/components/organisms/mypage/AuthButton.jsx
+++ src/components/organisms/mypage/AuthButton.jsx
@@ -1,22 +1,23 @@
-//import { useNavigate } from "react-router-dom";
+import { useRecoilValue } from "recoil";
+import { useNavigate } from "react-router-dom";
+
+import { authState } from "../../../store/auth";
 import { MypageButton } from "../../atoms/Mypage";
 
 const AuthButton = () => {
-  //const navigate = useNavigate();
-  //const auth = useContext(AuthContext);
+  const navigate = useNavigate();
+  const auth = useRecoilValue(authState);
 
-  /*
   const handleAuthClick = () => {
-    //if (auth.state !== 200) navigate('/profile/auth');
     console.log("auth test");
+    if (auth.state !== 200) navigate("/mypage/auth");
   };
-  */
 
-  return (
-    /* auth.state !== 200 ?
-		<MypageButton btnType="auth-42" onClick={handleAuthClick}>42인증</MypageButton> :
-          <MypageButton btnType="auth-42-done">인증완료</MypageButton>
-		  */
+  return auth.state !== 200 ? (
+    <MypageButton btnType="auth-42" onClick={handleAuthClick}>
+      42인증
+    </MypageButton>
+  ) : (
     <MypageButton btnType="auth-42-done">인증완료</MypageButton>
   );
 };
