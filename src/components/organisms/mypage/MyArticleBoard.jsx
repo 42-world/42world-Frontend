39bf28cf573e4112e62d529e1ab412f299b7f9c8
chichoon <chichoon.choi@gmail.com>
2022-04-30 19:38:12 +0900

    Feat: Article Board 게시물 가져오는부분, 마이페이지로 돌아가는부분 추가

--- src/components/organisms/mypage/MyArticleBoard.jsx
+++ src/components/organisms/mypage/MyArticleBoard.jsx
@@ -1,26 +1,70 @@
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 
+import { UserService } from "../../../network";
 import PreviewArticle from "../category";
 
 const MyArticleBoard = ({ isComment }) => {
+  const [articles, setArticles] = useState(null);
+  const navi = useNavigate();
+
+  const handleClickGoBack = () => {
+    navi("/mypage");
+  };
+
+  useEffect(() => {
+    const fetchMyArticles = async () => {
+      const response = isComment
+        ? await UserService.getMyComments()
+        : await UserService.getMyArticles();
+      setArticles(response.data.slice(0, 5));
+    };
+
+    fetchMyArticles();
+  }, [isComment]);
+
   return (
     <MyArticleWrapper>
-      <div className="board-header">
+      <div>
         <h1>{isComment ? "내 댓글" : "내 게시글"}</h1>
       </div>
+      <hr />
+      <div className="go-back" onClick={handleClickGoBack}>
+        {"< 돌아가기"}
+      </div>
+      <div className="article-list">{/* map -> article 표시 */}</div>
     </MyArticleWrapper>
   );
 };
 
 const MyArticleWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  border-radius: 0.3rem;
+  width: 100%;
+  height: fit-content;
+  margin: 1rem 0;
+  padding: 0.3rem;
+  border-radius: ${(props) => props.theme.borderRadius};
   box-shadow: ${(props) => props.theme.boxShadow};
-  background-color: #fff;
-  .articleList_content {
-    text-decoration: none;
-    color: ${(props) => props.theme.black};
+  h1 {
+    margin: 0.3rem 0.1rem 0.6rem 0.5rem;
+    height: fit-content;
+    font-size: 1.6rem;
+  }
+  hr {
+    color: ${(props) => props.theme.LineGray1};
+  }
+  .go-back {
+    margin: 0.3rem 0;
+    padding: 0.3rem;
+    &:hover {
+      cursor: pointer;
+    }
+  }
+  ${(props) => props.theme.mobileSize} {
+    box-shadow: none;
+    border-bottom: 1px solid ${(props) => props.theme.lineGray1};
+    border-radius: 0;
+    margin: 0;
   }
 `;
 
