f744384ddc3fb95ee23bde23a36d9db2be36d8e7
chichoon <chichoon.choi@gmail.com>
2022-04-30 22:40:56 +0900

    Delete: 안쓰는 useRef 삭제

--- src/components/organisms/mypage/MyArticleBoard.jsx
+++ src/components/organisms/mypage/MyArticleBoard.jsx
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import styled from "styled-components";
 
