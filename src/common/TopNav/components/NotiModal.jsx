dafc364f32c31b32de7b2a91d40fd4c0a967ed98
GulSam00 <gulsamcono@gmail.com>
2022-06-04 19:39:58 +0900

    Feat : figma에 맞게 디자인

--- src/common/TopNav/components/NotiModal.jsx
+++ src/common/TopNav/components/NotiModal.jsx
@@ -1,27 +1,36 @@
 import { Typography } from '@mui/material';
+
 import styled from 'styled-components';
 
 const tempData = [
   { title: '자유게시판', body: '제목입니다 글에 새로운 댓글이 달렸어요' },
+  { title: '자유게시판', body: '저 오늘 싸이봄ㅋㅋ 글이 글이asfasfasfas글이asfasfasfas 되었어요' },
   { title: '자유게시판', body: '저 오늘 싸이봄ㅋㅋ 글이 인기글이 되었어요' },
   { title: '자유게시판', body: '저 오늘 싸이봄ㅋㅋ 글이 인기글이 되었어요' },
   { title: '자유게시판', body: '저 오늘 싸이봄ㅋㅋ 글이 인기글이 되었어요' },
-  { title: '자유게시판', body: '저 오늘 싸이봄ㅋㅋ 글이 인기글이 되었어요' },
-  { title: '자유게시판', body: '저 오늘 싸이봄ㅋㅋ 글이 인기글이 되었어요' },
+  { title: '자유게시판', body: '저 오늘 싸이봄ㅋㅋ 글이asfasfasfas 인기글이 되었어요' },
   { title: '자유게시판', body: '저 오늘 싸이봄ㅋㅋ 글이 인기글이 되었어요' },
 ];
 const NotiModal = () => {
   return (
     <Typography>
       <ModalStyle>
-        <div className="main-title divide">알림</div>
+        <div className="main-title">알림</div>
+        <div className="divide"></div>
+
         <div className="notis">
           {tempData.map(data => {
             return (
-              <div className="noti">
-                <div className="title">{data.title}</div>
-                <div className="divide">{data.body}</div>
-              </div>
+              <>
+               <div className="noti">
+                  <img src="/assets/like.svg"/>
+                  <div className="body">
+                    <div className="title">{data.title}</div>
+                    <div className="text">{data.body}</div>
+                  </div>
+                </div>
+                <div className="divide"></div>
+              </>
             );
           })}
         </div>
@@ -36,12 +45,13 @@ const ModalStyle = styled.div`
   font-size: 20px;
   max-width: 300px;
   max-height: 400px;
+  padding : 10px;
 
-  .main-title {
-    width: 290px;
-  }
+
   .divide {
-    border-bottom: 1px solid gray;
+    border-bottom: 2px solid  #D9D9D9;
+    width: 270px;
+
   }
   .title,
   .main-title {
@@ -50,20 +60,33 @@ const ModalStyle = styled.div`
   }
 
   .notis {
-    max-height: 350px;
+    max-height: 300px;
     overflow: scroll;
-    overflow-x: hidden;
+    overflow-x: hidden;    
   }
 
   .notis::-webkit-scrollbar {
-    width: 8px;
+    width: 5px;
   }
 
   .notis::-webkit-scrollbar-thumb {
     border-radius: 10px;
-    background-color: gray;
+    background-color: #D9D9D9;
   }
 
   .notis::-webkit-scrollbar-track {
+  }
+
+  .noti {
+    display : flex;
+    max-height : 120px;
+    * {
+      margin :5px;
+      overflow : hidden;
+    }
+    img { 
+      min-width : 30px;
+      
+    }
   }
 `;
