f22e622f528597f3b69f8ba47f6d8b5cd277ed7b
Luna <luvbliny@gmail.com>
2022-06-06 18:48:10 +0900

    Refactor: profile image mapping
    - api를 통해 받아온 유저의 이미지 넘버와 실제 이미지 맵핑
    
    Co-authored-by: rockpell <aythffk@gmail.com>

--- src/common/TopNav/components/UserName.jsx
+++ src/common/TopNav/components/UserName.jsx
@@ -1,14 +1,15 @@
 import { Link } from 'react-router-dom';
 import { StyledProfileImage, StyledUserName } from '../styled';
+import { PROFILE_LIST } from 'common/constants';
 
 const UserName = ({ user }) => {
+  console.log(user);
   return (
     <Link to="/mypage">
       <StyledUserName>
         <div>{user.nickname}</div>
         <StyledProfileImage
-          // TODO : 사진 변경
-          src="/assets/CharacterWhiteBG/babybbo.png"
+          src={`/assets/CharacterWhiteBG/${PROFILE_LIST[user.character]}`}
           alt="사용자 캐릭터"
           width="50px"
           height="50px"
---
+++ src/common/constants.js
@@ -0,0 +1,14 @@
+// TODO: file naming을 인덱스로 관리하면 좀 더 편할 것 같음
+export const PROFILE_LIST = {
+  0: 'bbo.png',
+  1: 'bora.png',
+  2: 'ddub.png',
+  3: 'nana.png',
+  4: 'bongsuni.png',
+  5: 'hyeonkim.png',
+  6: 'babybbo.png',
+  7: 'babynana.png',
+  8: 'babybora.png',
+  9: 'babyddub.png',
+  10: 'babyhyeonkim.png',
+};
