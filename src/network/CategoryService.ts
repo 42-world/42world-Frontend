70e80ad7907323e9acb69402726a195436630745
rockpell <rockpell.cleaning@gmail.com>
2022-06-19 22:50:50 +0900

    Chore: CategoryService, return type 명시

--- src/network/CategoryService.ts
+++ src/network/CategoryService.ts
@@ -1,9 +1,12 @@
+import { AxiosResponse } from 'axios';
 import { apiClient } from './APIType';
 import { GetCategoriesResponse } from './types/Category';
 
 export const CATEGORY_API_URL = '/categories';
 
-export const getCategories = async () => {
+export const getCategories = async (): Promise<
+  AxiosResponse<GetCategoriesResponse, any>
+> => {
   return apiClient.get<GetCategoriesResponse>(CATEGORY_API_URL);
 };
 
