e942533ff09d81edcc79f31fc223d30d8461c84d
Luna <luvbliny@gmail.com>
2022-06-28 21:40:46 +0900

    Style: search input css in main page

--- src/components/pages/main/Main.jsx
+++ src/components/pages/main/Main.jsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import QuickLink from 'common/QuickLink/QuickLink';
 import MainPreviewBoards from './MainPreviewBoards';
+import { FaSearch } from 'react-icons/fa';
 
 const Main = () => {
   return (
@@ -8,7 +9,10 @@ const Main = () => {
       <StyledMain>
         <StyledMainContent>
           <StyledMainSection>
-            <StyledInput type="text" placeholder="검색어를 입력해주세요" />
+            <SearchForm>
+              <FaSearch />
+              <StyledInput type="text" placeholder="검색어를 입력해주세요" />
+            </SearchForm>
             <MainPreviewBoards />
           </StyledMainSection>
 
@@ -21,6 +25,18 @@ const Main = () => {
     </>
   );
 };
+
+const SearchForm = styled.form`
+  color: white;
+  display: flex;
+  align-items: center;
+  position: relative;
+  svg {
+    height: 25px;
+    transform: translateX(45px);
+    color: black;
+  }
+`;
 
 const StyledMain = styled.div`
   display: flex;
@@ -36,11 +52,17 @@ const StyledMainSection = styled.div`
 `;
 
 const StyledInput = styled.input`
-  width: 800px; // TODO: reactive width
-  height: 40px;
-  border-radius: 10px;
+  width: 800px;
+  height: 50px;
+  border-radius: 25px;
   margin: 10px;
-  padding: 10px;
+
+  transform-origin: right center;
+  padding: 5px 10px;
+  padding-left: 40px;
+  font-size: 16px;
+  background-color: transparent;
+  border: 2px solid black;
 `;
 
 export default Main;
