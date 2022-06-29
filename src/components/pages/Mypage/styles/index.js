a0e62a2f9c33782291d5d50d8aa6405d805534cf
chichoon <chichoon.choi@gmail.com>
2022-06-30 01:27:47 +0900

    Rename: profle => profile 오타...

--- src/components/pages/Mypage/styles/index.js
+++ src/components/pages/Mypage/styles/index.js
@@ -3,7 +3,7 @@ import StyledMypageButton from './MypageButton.styles';
 import StyledMyArticlePreview from './MyArticlePreview.styles';
 import StyledMyArticleBoard from './MyArticleBoard.styles';
 
-import StyledMypageProfile from './MypageProfle.styles';
+import StyledMypageProfile from './MypageProfile.styles';
 import StyledProfileSection from './ProfileSection.styles';
 import StyledCharSelectModal from './CharSelectModal.styles';
 
