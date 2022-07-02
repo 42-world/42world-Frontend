c73caa0dda0423f981b152670ffdb980a65eec21
PIut0 <slimdoks@gmail.com>
2022-07-02 14:32:33 +0900

    Design: DOM 구조 변경 및 배경 이미지 추가

--- src/components/pages/login/Login.jsx
+++ src/components/pages/login/Login.jsx
@@ -9,34 +9,45 @@ const Login = () => {
   const { onClickButton, isAuthCallbackProcess } = useLogin();
 
   return (
-    <div>
-      <div>
-        <img src="assets/images/logo/Logo@1x.png" alt="Logo" />
-      </div>
-      <div>
+    <Container>
+      <StyledLogin>
         <div>
-          <p>42WORLD 커뮤니티는</p>
-          <p>
-            42서울인들과
-            <HighlightText>취업자/창업자/알럼나이/카뎃을</HighlightText>
-          </p>
-          <p>아우르는 통합 커뮤니티 채널입니다.</p>
-          <p>여러분의 피드백을 받아</p>
-          <p>지속적인 업데이트가 이루어 집니다.</p>
+          <img src="assets/images/logo/Logo@1x.png" alt="Logo" />
         </div>
         <div>
-          {isAuthCallbackProcess() ? (
-            <CircularProgress />
-          ) : (
-            <button onClick={onClickButton}>Github Login</button>
-          )}
+          <div>
+            <p>42WORLD 커뮤니티는</p>
+            <p>
+              42서울인들과
+              <HighlightText>취업자/창업자/알럼나이/카뎃을</HighlightText>
+            </p>
+            <p>아우르는 통합 커뮤니티 채널입니다.</p>
+            <p>여러분의 피드백을 받아</p>
+            <p>지속적인 업데이트가 이루어 집니다.</p>
+          </div>
+          <div>
+            {isAuthCallbackProcess() ? <CircularProgress /> : <button onClick={onClickButton}>Github Login</button>}
+          </div>
         </div>
-      </div>
-    </div>
+      </StyledLogin>
+    </Container>
   );
 };
 
 export default Login;
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: 100vw;
+  height: 100vh;
+
+  background: url('/assets/videos/42seoul_login.jpg') no-repeat center center/cover;
+`;
+
+const StyledLogin = styled.div``;
 
 const HighlightText = styled.span`
   color: ${props => props.theme.textBlue};
