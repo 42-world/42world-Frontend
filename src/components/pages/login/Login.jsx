b31b37bfe85cbfda0b43e5c123df6234d0f6c9e5
PIut0 <slimdoks@gmail.com>
2022-07-02 14:56:40 +0900

    Design: 로그인 블럭 디자인 적용
    done: #57

--- src/components/pages/login/Login.jsx
+++ src/components/pages/login/Login.jsx
@@ -11,24 +11,23 @@ const Login = () => {
   return (
     <Container>
       <StyledLogin>
-        <div>
-          <img src="assets/images/logo/Logo@1x.png" alt="Logo" />
-        </div>
-        <div>
-          <div>
-            <p>42WORLD 커뮤니티는</p>
-            <p>
-              42서울인들과
-              <HighlightText>취업자/창업자/알럼나이/카뎃을</HighlightText>
-            </p>
-            <p>아우르는 통합 커뮤니티 채널입니다.</p>
-            <p>여러분의 피드백을 받아</p>
-            <p>지속적인 업데이트가 이루어 집니다.</p>
-          </div>
-          <div>
-            {isAuthCallbackProcess() ? <CircularProgress /> : <button onClick={onClickButton}>Github Login</button>}
-          </div>
-        </div>
+        <img src="assets/images/logo/Logo@1x.png" alt="Logo" />
+        <h1>당신이 있기에 행복한 42Seoul</h1>
+        <LoginTextBox>
+          <TextContent>42WORLD 커뮤니티는</TextContent>
+          <TextContent>
+            42서울인들과
+            <HighlightText>취업자/창업자/알럼나이/카뎃을</HighlightText>
+          </TextContent>
+          <TextContent>아우르는 통합 커뮤니티 채널입니다.</TextContent>
+          <TextContent>여러분의 피드백을 받아</TextContent>
+          <TextContent>지속적인 업데이트가 이루어 집니다.</TextContent>
+          {isAuthCallbackProcess() ? (
+            <CircularProgress />
+          ) : (
+            <LoginButton onClick={onClickButton}>Github 로그인</LoginButton>
+          )}
+        </LoginTextBox>
       </StyledLogin>
     </Container>
   );
@@ -45,11 +44,53 @@ const Container = styled.div`
   height: 100vh;
 
   background: url('/assets/videos/42seoul_login.jpg') no-repeat center center/cover;
+
+  img {
+    width: 15rem;
+  }
+
+  h1 {
+    font-size: 1.2rem;
+    font-weight: bold;
+    margin-bottom: 2rem;
+  }
 `;
 
-const StyledLogin = styled.div``;
+const StyledLogin = styled.div`
+  color: ${props => props.theme.textWhite};
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const LoginTextBox = styled.div`
+  padding: 2rem;
+  border-radius: 1rem;
+  background-color: ${props => props.theme.backgroundWhite};
+`;
+
+const TextContent = styled.p`
+  color: ${props => props.theme.textBlack};
+  text-align: center;
+  font-weight: bold;
+  margin-bottom: 0.4rem;
+`;
 
 const HighlightText = styled.span`
   color: ${props => props.theme.textBlue};
   font-weight: 700;
+`;
+
+const LoginButton = styled.button`
+  width: calc(100% - 4rem);
+
+  margin: 1rem 2rem 0 2rem;
+  padding: 0.4rem;
+
+  border: none;
+  border-radius: 0.5rem;
+
+  color: ${props => props.theme.textWhite};
+  background-color: #2a2d38;
 `;
