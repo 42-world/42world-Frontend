2d3e16a0ddb6e1af740fd16377f88e1cd1b6421a
rockpell <aythffk@gmail.com>
2022-07-13 01:16:30 +0900

    Feat: Footer 아이콘 적용

--- src/common/Footer/Footer.tsx
+++ src/common/Footer/Footer.tsx
@@ -2,14 +2,25 @@
 
 import { css } from '@emotion/react';
 import BuyCoffee from './BuyCoffee';
+import { BsGithub } from 'react-icons/bs';
+import { FiMail } from 'react-icons/fi';
+import { BiMessageAltDots } from 'react-icons/bi';
+import { theme } from '@root/styles/theme';
+
+const GITHUB_URL = 'https://github.com/42-world';
+const EMAIL = '42world.official@gmail.com';
 
 const Footer = () => {
   return (
     <footer css={footerStyle}>
-      <address>
-        <button>a</button>
-        <button>b</button>
-        <button>c</button>
+      <address css={iconAddressStyle}>
+        <a href={GITHUB_URL} target={'_blank'} rel="noreferrer">
+          <BsGithub css={iconColor} size={20} />
+        </a>
+        <a href={`mailto:${EMAIL}`}>
+          <FiMail css={iconColor} size={20} />
+        </a>
+        <BiMessageAltDots css={iconColor} size={20} />
       </address>
       <address>
         <BuyCoffee />
@@ -24,12 +35,22 @@ const Footer = () => {
 const footerStyle = css`
   display: flex;
   flex-direction: column;
-  gap: 6px;
+  gap: 12px;
 
   margin-top: 16px;
   margin-bottom: 6px;
 
   text-align: center;
+`;
+
+const iconAddressStyle = css`
+  display: flex;
+  justify-content: center;
+  gap: 12px;
+`;
+
+const iconColor = css`
+  color: ${theme.backgroundBlack};
 `;
 
 const strongText = css`
