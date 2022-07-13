28c96f82f9728d1a1b2ab0f03759f56bfcf9136d
rockpell <aythffk@gmail.com>
2022-07-14 02:32:20 +0900

    Feat: 이메일 인증 페이지에 배경화면 적용
    - 글자가 겹쳐보여서 추후 이미지 수정 필요

--- src/components/pages/Auth.jsx
+++ src/components/pages/Auth.jsx
@@ -4,59 +4,31 @@ import Seoul42 from '../organisms/auth/Seoul42';
 const Auth = ({ isCallback }) => {
   return (
     <AuthBlock>
-      <div className="video-container">
-        <video src="assets/videos/video-1.mp4" type="video/mp4" autoPlay loop muted />
-        <Seoul42 />
-      </div>
+      <StyledImg src="assets/main_background.webp" />
+      <Seoul42 />
     </AuthBlock>
   );
 };
 
+const StyledImg = styled.img`
+  object-fit: cover;
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  z-index: -1;
+`;
+
 const AuthBlock = styled.div`
-  background: url('/images/img-home.jpg') center center/cover no-repeat;
-  @media screen and (min-width: 960px) {
-    .video-container {
-      position: fixed;
-      scrollbar-width: none;
+  position: fixed;
+  scrollbar-width: none;
 
-      height: 100vh;
-      width: 100%;
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      align-items: center;
-      object-fit: contain;
-      video {
-        object-fit: cover;
-        width: 100%;
-        height: 100%;
-        position: fixed;
-        z-index: -1;
-      }
-    }
-  }
-
-  @media screen and (max-width: 960px) {
-    .video-container {
-      position: fixed;
-      scrollbar-width: none;
-
-      height: 100vh;
-      width: 100%;
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      align-items: center;
-      object-fit: contain;
-      video {
-        object-fit: cover;
-        width: 100%;
-        height: 100%;
-        position: fixed;
-        z-index: -1;
-      }
-    }
-  }
+  height: 100vh;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  object-fit: contain;
 `;
 
 export default Auth;
