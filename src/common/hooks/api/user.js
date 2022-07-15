4f0011477f42a8b8115a1b8083f17839069eac42
rockpell <aythffk@gmail.com>
2022-07-16 00:43:13 +0900

    Fix: login 중복으로 시도하던 문제 수정
    Co-authored-by: blingblin-g <luvbliny@gmail.com

--- src/common/hooks/api/user.js
+++ src/common/hooks/api/user.js
@@ -1,11 +1,20 @@
-import { useQuery } from 'react-query';
+import { useQuery, useQueryClient } from 'react-query';
 import { UserService } from '@network';
 
 export const USER_URL = '/user';
 export const USERS_URL = '/users';
 export const USERS_ME_URL = '/users/me';
 
-export const getUser = () => {
+export const useGetUser = () => {
   const { isError, data } = useQuery([USERS_ME_URL], UserService.getUser);
   return { isError, user: data?.data ?? {} };
+};
+
+export const useGetClearUser = () => {
+  const queryClient = useQueryClient();
+
+  return () => {
+    queryClient.removeQueries(USERS_ME_URL);
+    queryClient.clear();
+  };
 };
