ac222b821a1e16365f8364cc32ed2e1db93a39f1
PIut0 <slimdoks@gmail.com>
2022-07-15 22:31:10 +0900

    Design: 메일인증 페이지 디자인 수정

--- src/components/pages/Auth.jsx
+++ src/components/pages/Auth.jsx
@@ -4,23 +4,16 @@ import Seoul42 from '../organisms/auth/Seoul42';
 const Auth = ({ isCallback }) => {
   return (
     <AuthBlock>
-      <StyledImg src="assets/main_background.jpeg" />
       <Seoul42 />
     </AuthBlock>
   );
 };
 
-const StyledImg = styled.img`
-  object-fit: cover;
-  width: 100%;
-  height: 100%;
-  position: fixed;
-  z-index: -1;
-`;
-
 const AuthBlock = styled.div`
   position: fixed;
   scrollbar-width: none;
+
+  background: url('assets/main_background.jpeg') no-repeat center center/cover;
 
   height: 100vh;
   width: 100%;
