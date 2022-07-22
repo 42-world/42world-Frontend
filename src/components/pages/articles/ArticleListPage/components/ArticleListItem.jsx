6bc26b7a4207843e620959412b88d706418149cc
huni <petercha2000@gmail.com>
2022-07-23 03:59:01 +0900

    debug: 잘 모르는 코드 질문

--- src/components/pages/articles/ArticleListPage/components/ArticleListItem.jsx
+++ src/components/pages/articles/ArticleListPage/components/ArticleListItem.jsx
@@ -8,6 +8,7 @@ import CategoryName from './CategoryName';
 const ArticleListItem = ({ article, categoryId, categories }) => {
   return (
     <>
+      {/* TODO: 이거 무슨 코드인가요 */}
       {categoryId ? <></> : <CategoryName article={article} categories={categories} />}
       <Link to={`/article/${article.id}`} className="articleList_content" key={`article_${article.id}`}>
         <PreviewArticle article={article} />
