7aedddaab06bc2f924ff6b75692b9240229fd709
chichoon <chichoon.choi@gmail.com>
2022-07-30 19:19:32 +0900

    Feat: 마이페이지 메인에서 useQuery로 데이터 3종류 받아오도록 훅 설정

--- src/components/pages/Mypage/hooks/useMypage.js
+++ src/components/pages/Mypage/hooks/useMypage.js
@@ -2,6 +2,7 @@ import { useNavigate, useParams } from 'react-router-dom';
 import { useEffect } from 'react';
 
 import constants from '@components/pages/Mypage/constants';
+import { useGetMyArticles, useGetMyComments, useGetLikeArticles } from '@components/pages/Mypage/hooks';
 
 const useMypage = () => {
   const params = useParams();
@@ -13,11 +14,15 @@ const useMypage = () => {
       liked: constants.LIKED,
     }[params.articleType] || 0;
 
+  const { articles } = useGetMyArticles(1);
+  const { comments } = useGetMyComments(1);
+  const { likeArticles } = useGetLikeArticles(1);
+
   useEffect(() => {
     if (!articleType && params.articleType) navigate('/error');
-  }, [params, articleType]);
+  }, [params, articleType, navigate]);
 
-  return { articleType };
+  return { articleType, articles: articles.slice(5), comments: comments.slice(5), likeArticles: likeArticles.slice(5) };
 };
 
 export default useMypage;
