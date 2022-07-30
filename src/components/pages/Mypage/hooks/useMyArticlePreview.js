bb74007927170dc70566b8453f3022f248c4ae03
chichoon <chichoon.choi@gmail.com>
2022-07-30 19:19:40 +0900

    Delete: 안쓰는 import 삭제

--- src/components/pages/Mypage/hooks/useMyArticlePreview.js
+++ src/components/pages/Mypage/hooks/useMyArticlePreview.js
@@ -1,7 +1,4 @@
-import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-
-import getArticleInfo from '@components/pages/Mypage/utils/myArticlePreviewUtils';
 
 const useMyArticlePreview = hrefLink => {
   const navigate = useNavigate();
