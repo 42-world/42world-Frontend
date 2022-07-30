2e30415ac37932761922ad6bd24fb68d193e9469
chichoon <chichoon.choi@gmail.com>
2022-07-30 16:48:01 +0900

    Fix: 반환 변수명 잘못적어서 데이터 안받아와지던 문제 수정 (바본가)

--- src/components/pages/Mypage/hooks/useGetLikeArticles.ts
+++ src/components/pages/Mypage/hooks/useGetLikeArticles.ts
@@ -2,7 +2,9 @@ import { UserService } from '@root/network';
 import { useQuery } from 'react-query';
 
 const useGetLikeArticles = (page: number) => {
-  const { isError, data, refetch } = useQuery(['myLikes'], () => UserService.getLikeArticles);
+  const { isError, data = { data: [] }, refetch } = useQuery(['myLikes'], () => UserService.getLikeArticles(page));
+
+  return { isError, likeArticles: data.data, refetch };
 };
 
 export default useGetLikeArticles;
--- src/components/pages/Mypage/hooks/useGetMyArticles.ts
+++ src/components/pages/Mypage/hooks/useGetMyArticles.ts
@@ -2,11 +2,15 @@ import { UserService } from '@root/network';
 import { useQuery } from 'react-query';
 
 const useGetMyArticles = (page: number) => {
-  const { isError, data, refetch } = useQuery(['myArticles', page], () => UserService.getMyArticles(page), {
+  const {
+    isError,
+    data = { data: [] },
+    refetch,
+  } = useQuery(['myArticles', page], () => UserService.getMyArticles(page), {
     refetchOnWindowFocus: true,
   }); // TODO: 게시글 미리보기에서 게시글 모음 페이지로 넘어갈 때 refetch 필요성?
 
-  return { isError, articles: data?.data, refetch };
+  return { isError, articles: data.data, refetch };
 };
 
 export default useGetMyArticles;
