179e014e90b0250fd7451f29c3a868f277656334
PIut0 <slimdoks@gmail.com>
2022-08-15 08:59:27 +0900

    Feat: 알림 텍스트 대신 아이콘으로 변경

--- src/common/TopNav/components/UserItems.jsx
+++ src/common/TopNav/components/UserItems.jsx
@@ -1,13 +1,17 @@
+/** @jsxImportSource @emotion/react */
+
+import { css } from '@emotion/react';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+
 import { StyledMenuButton } from '../styled';
 import { useGetUser } from '@common/hooks/api/user';
 import { isEmpty } from '@common/utils';
 import LoginButton from './LoginButton';
 import UserName from './UserName';
-
 const UserItems = ({ onClick }) => {
   const { user } = useGetUser();
   return (
-    <div className="user">
+    <div css={UserItemsStyle}>
       {isEmpty(user) ? (
         <>
           <LoginButton />
@@ -15,9 +19,7 @@ const UserItems = ({ onClick }) => {
       ) : (
         <>
           {/* TODO : 아이콘 및 모달 적용 */}
-          <StyledMenuButton className="alarm-button" onClick={onClick}>
-            알람
-          </StyledMenuButton>
+          <NotificationsIcon css={UserItemNotificationStyle} onClick={onClick} />
           <StyledMenuButton>
             <UserName user={user} />
           </StyledMenuButton>
@@ -26,5 +28,18 @@ const UserItems = ({ onClick }) => {
     </div>
   );
 };
+
+const UserItemsStyle = css`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`;
+
+const UserItemNotificationStyle = css`
+  width: 2rem;
+  height: 2rem;
+  margin-right: 0.5rem;
+`;
 
 export default UserItems;
