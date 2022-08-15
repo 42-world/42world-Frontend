337c4f56281c4f79e6516b349e5bf369fc831d9f
chichoon <chichoon.choi@gmail.com>
2022-08-15 14:19:38 +0900

    Refactor: ArticlePreview emotion으로 교체 및 tsx로 수정

---
+++ src/components/pages/Mypage/components/MypageBoard/ArticlePreview.tsx
@@ -0,0 +1,46 @@
+/** @jsxImportSource @emotion/react */
+
+import { useNavigate } from 'react-router-dom';
+import { AiOutlineHeart } from 'react-icons/ai';
+import { BiCommentDots } from 'react-icons/bi';
+
+import {
+  articlePreviewWrapper,
+  articleHeaderWrapper,
+  reactionWrapper,
+  like,
+  comment,
+} from '@components/pages/Mypage/styles/ArticlePreview.styles';
+
+interface IProps {
+  id: number;
+  title: string;
+  likeCount: number;
+  commentCount: number;
+}
+
+const ArticlePreview = ({ id, title, likeCount, commentCount }: IProps) => {
+  const navigate = useNavigate();
+
+  const handleClickArticle = () => {
+    navigate(`/article/${id}`);
+  };
+
+  return (
+    <div css={articlePreviewWrapper} onClick={handleClickArticle}>
+      <h3 css={articleHeaderWrapper}>{title}</h3>
+      <div css={reactionWrapper}>
+        <span css={like}>
+          <AiOutlineHeart />
+          <span>{likeCount}</span>
+        </span>
+        <span css={comment}>
+          <BiCommentDots />
+          <span>{commentCount}</span>
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default ArticlePreview;
