cf937ea0eab849e03515585f8aee303313ef188d
rockpell <aythffk@gmail.com>
2022-08-20 21:24:30 +0900

    Fix: 게시글, 댓글, 검색, 캐시타임 1초로 변경
    - 캐시 타임이 길어서 갱신이 안되는경우가 있어서 짧게 가져가는것으로 변경
    - 캐시타임을 완전 없애거나 refetchOnMount 옵션을 활성화하면 것은 피함
    - 하위 컴포넌트에서 useQuery를 여러번 호출할때 api가 여러번 호출 될수 있기 떄문

--- src/common/hooks/api/comment.ts
+++ src/common/hooks/api/comment.ts
@@ -2,8 +2,12 @@ import { CommentService2 } from '@root/network';
 import { useQuery } from 'react-query';
 
 export const useGetComments = (articleId: number, page: number, take?: 10) => {
-  const { isError, data, refetch } = useQuery(['comments', articleId, page], () =>
-    CommentService2.getCommentsByArticleId(articleId, page, take),
+  const { isError, data, refetch } = useQuery(
+    ['comments', articleId, page],
+    () => CommentService2.getCommentsByArticleId(articleId, page, take),
+    {
+      cacheTime: 1,
+    },
   );
 
   return { isError, comments: data?.data, meta: data?.meta, refetch };
