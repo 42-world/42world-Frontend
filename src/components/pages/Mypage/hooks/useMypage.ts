04f9526018976515e2bd9ad96501cceea91f70a0
chichoon <chichoon.choi@gmail.com>
2022-08-24 01:09:06 +0900

    Feat: 로그인 안 되어있을 때 mypage 막아둠 (임시)

--- src/components/pages/Mypage/hooks/useMypage.ts
+++ src/components/pages/Mypage/hooks/useMypage.ts
@@ -1,9 +1,11 @@
 import { useNavigate, useParams } from 'react-router-dom';
+import { useGetUser } from '@common/hooks/api/user';
 import { useEffect } from 'react';
 
 import { ARTICLE_TYPE } from '@components/pages/Mypage/utils';
 
 const useMypage = () => {
+  const { user } = useGetUser();
   const params = useParams();
   const navigate = useNavigate();
   const articleType =
@@ -14,6 +16,7 @@ const useMypage = () => {
     }[params.articleType ?? 'myPage'] || 0;
 
   useEffect(() => {
+    if (!user || !user.nickname) navigate('/');
     if (!articleType && params.articleType) navigate('/error');
   }, [params, articleType, navigate]);
 
