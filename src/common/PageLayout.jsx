e548d6d1daaeeb02f52e7325c5879974cfa8f1ad
huni <petercha2000@gmail.com>
2022-09-25 01:10:16 +0900

    feate: PageLayout 에 헬맷등록

--- src/common/PageLayout.jsx
+++ src/common/PageLayout.jsx
@@ -1,10 +1,17 @@
 import { Outlet } from 'react-router-dom';
 import TopNav from '@common/TopNav';
 import Footer from '@common/Footer';
+import ReactHelmet from './ReactHelmet';
 
 export const PageLayout = () => {
   return (
     <>
+      <ReactHelmet
+        keywords="42World"
+        title="42World"
+        description="42서울 재학생과 졸업생 모두를 아우르는 커뮤니티"
+        favicon="/assets/42worldCharacterLogo.png"
+      />
       <TopNav />
       <Outlet />
       <Footer />
